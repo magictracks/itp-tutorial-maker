2f25a35fb2e79b88654bf099e05b610d9df8d136
joeyklee <j@jk-lee.com>
2018-08-09 11:31:44 -0400

    updated save html output

--- client/components/SaveAsHTML.js
+++ client/components/SaveAsHTML.js
@@ -11,6 +11,7 @@ class SaveAsHTML extends Component {
 
     this._onclick = this._onclick.bind(this);
     this.local = this.state.components[name] = {}
+    this.renderTutorial = this.renderTutorial.bind(this);
 
   }
 
@@ -19,16 +20,64 @@ class SaveAsHTML extends Component {
       return true
   }
 
-
   _onclick(e) {
     this.emit("projects:saveAsJSON");
   }
+
+  renderTutorial(){
+
+    // let sections = html`
+    //  ${this.state.sections.map( (section) => `<div class="section"><h3>${section.properties.title}</h3><h4>${section.properties.description}</h4></div>`)}
+    // `
+
+    let output = html`
+    <html>
+      <head>
+        <title>Magic Tracks!</title>
+      </head>
+      <body>
+        <div id="Tutorial">
+          <header>
+            <h1>${this.state.tutorial.title}</h1>
+            <h2>${this.state.tutorial.description}</h2>
+          </header>
+          <main>
+            <h2>Sections </h2>
+            ${this.state.sections.map( (section) => html`
+              <div class="section">
+                <h3>${section.properties.title}</h3>
+                <h4>${section.properties.description}</h4>
+                <div class="resources">
+                  <h3>Resources</h3>
+                  
+                  ${section.resources.map( (resource) => html`
+                  <div class="resource">
+                    <h3>${resource.properties.title}</h3>
+                    <h4>${resource.properties.description}</h4>
+                  </div>
+                  `
+                  )}
+                </div>
+              </div>
+              `
+              )}
+          </main>
+        </div>
+      </body>
+    </html>
+    `
+
+    // console.log(output);
+    return encodeURIComponent(output.outerHTML)
+  }
+
+
 
   createElement () {
     // encodeURIComponent(document.querySelector("main").outerHTML)
     return html`
       <div class="pa2 mr2 self-end tr">
-        <a class="tr" href="data:text/html;charset=utf-8,<h1>this will be your tutorial output in HTML</h1>" download="${this.state.tutorial.id}.html" onclick=${this._onclick}>Save as HTML</a>
+        <a class="tr" href="data:text/html;charset=utf-8,${this.renderTutorial()}" download="${this.state.tutorial.id}.html" onclick=${this._onclick}>Save as HTML</a>
       </div>
     `
   }
